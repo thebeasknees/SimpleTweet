d5aea268371f39461a32e57abcbe31cb717be8eb
Keven Beasley <hottsauce0216@gmail.com>
2021-09-30 22:23:08 -0500

    Added endless recycler in order to scroll through tweets infinitely

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -11,6 +11,7 @@ public class Tweet {
 
     public String body;
     public String createdAt;
+    public long id;
     public User user;
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
@@ -18,6 +19,7 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.id = jsonObject.getLong("id");
         return tweet;
     }
 
